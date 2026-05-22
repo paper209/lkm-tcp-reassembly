4bd55d730dfa96e86ceb2b1a7060023bba1f2ed0
paper209 <fleshed-stroll84@icloud.com>
2026-05-23 06:45:35 +0900

    Add configuration definitions in config.h

---
+++ config.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define SET_ADDR 0x7F000001
+#define SET_PORT 209
+
+#define TCP_MAX_BUFFER 2048
+#define TCP_MAX_SESSIONS 1024
+
+//#define FILTER_MAX_LENGTH 30
+
+#endif
